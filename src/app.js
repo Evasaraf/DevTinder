82a27c8a89d5b2c8289a38320610a14284ea1258
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-07-28 11:17:18 +0530

    exploring get post commit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    res.send("hello from the server");
+app.use("/" ,(req, res) => {
+    res.send("hi eva ");
 });
 app.use("/greet" ,(req, res) => {
     res.send("hello mam");
 });
+app.use((req, res) => {
+    res.send("hello from the server");
+});
+
 app.listen(7777, ()=> {
     console.log("server is successfully litening on port 7777")
 });
