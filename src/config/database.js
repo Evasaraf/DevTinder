ce340860cf8a96b7058564ea8cd5557ea8b89b5c
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-08-06 23:37:26 +0530

    DNS issue resolved by removing srv

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async() => {
-    await mongoose.connect("mongodb+srv://evasaraf:baalbaalbache9691@namastenode.z6veqq3.mongodb.net/devTinder");
+    await mongoose.connect("mongodb://evasaraf:baalbaalbache9691@ac-yugxtsy-shard-00-00.z6veqq3.mongodb.net:27017,ac-yugxtsy-shard-00-01.z6veqq3.mongodb.net:27017,ac-yugxtsy-shard-00-02.z6veqq3.mongodb.net:27017/?ssl=true&replicaSet=atlas-gbfbrx-shard-0&authSource=admin&appName=Namastenode");
    // mongodb+srv://evasaraf:baalbaalbache9691@namastenode.z6veqq3.mongodb.net/
 };
 
