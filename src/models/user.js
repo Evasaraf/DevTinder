8ef286933b972841c7c4714a8e51013e6b7d902a
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-08-10 16:17:06 +0530

    understanding user validation

--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,43 @@ const mongoose = require("mongoose");
 // creating a user schema 
 const userschema = mongoose.Schema({
     firstname : {
-        type : String
+        type : String,
+        required : true,
+        minlength : 3,
 },
     lastname : {
         type: String
     },
     
     password: {
-        type : String
+        type : String,
+        required : true,
+        lowercase : true,
     },
 
     age:{
-        type: Number
+        type: Number,
+        min : 18,
     },
 
     gender:{
+        type: String,
+        validate(value){
+            if(value!="male" && value!="female" && value!="other"){
+                throw new Error("gender should be male, female or other");
+            }
+        }
+    },
+    
+    profileUrl: {
         type: String
+    },
+    about:{
+        type: String
+    },
+    skills: {
+        type: [String]
     }
-
 
 })
 // creating a user model
