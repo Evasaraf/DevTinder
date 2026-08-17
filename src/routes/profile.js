617c2290ac5009ee06cfd0d92b0e2bf317a50a78
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-08-17 20:55:04 +0530

    creating a new profilerouter to handle tasks for user profile

---
+++ src/routes/profile.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const profileRouter = express.Router;
+const userauth = require("./middleware/auth.js")
+
+profileRouter.get("/profile",  userauth, async (req, res)=>{
+    try{
+  const user = req.user;
+  res.send(user);
+}
+catch(err){
+  res.send("something went wrong while fetching the profile data" + err.message);
+}
+});
+
+module.exports = profileRouter;
