071ff926aea63382a661c5a3923191dfe3698bf3
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-08-19 16:28:36 +0530

    saving the updated data in mongodb database

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -19,13 +19,16 @@ profileRouter.patch("/profile/edit", userauth, async (req, res)=>{
     if(!validateEditProfileData(req)){
         throw new Error("Invalid update request");
     }
-    const loggedInUser = req.user;
-    console.log("user orignal details without update:", loggedInUser);// it will send 
+    const loggedInUser = req.user;// 
+    console.log("user orignal details without update:", loggedInUser);// give the details of loggedin user
 
-    Object.keys(req.body).forEach((key)=>{
+    Object.keys(req.body).forEach((key)=>{// modify the fields, as given in request body of postman
         loggedInUser[key] = req.body[key];
     });
-    console.log("user details are updated:" , loggedInUser);
+    console.log("user details are updated:" , loggedInUser);// give the updated details of loggedinuser
+    
+    await loggedInUser.save();// make  the changes in mongodb also and save 
+
    res.send(`${loggedInUser.firstname}, your profile updated successfully`);
  }
  catch(err){
