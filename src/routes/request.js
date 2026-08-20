a9179a606ac1f00ff2a71fa0b90a9a9b44620962
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-08-20 20:25:51 +0530

    allow only one user to send a request to other user

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,7 +2,8 @@ const express = require("express");
 const connectionrequestRouter = express.Router();
 const {userauth} = require("../middleware/auth");
 const user = require("../models/user");
-const connectionRequest = require("../models/connectionrequest")
+const connectionRequest = require("../models/connectionrequest");
+const { isAbaRouting } = require("validator");
 
 
 connectionrequestRouter.post("/sendconnectionrequest", userauth, async (req, res)=>{
@@ -18,6 +19,21 @@ connectionrequestRouter.post("/request/send/:status/:toUserId", userauth, async(
     const toUserId = req.params.toUserId;
     const status = req.params.status;
 
+   const allowedstatus = ["ignore" , "interested"];
+   if(!allowedstatus.includes(status)){
+    return res.status(400).json({message: "invalid status type " + status});
+   }
+   // if a has sent a connection request to b once, it should not be allowed to send it again, and also b can not send request to a then
+   const existingconnectionRequest = await connectionRequest.findOne({
+    $or:[
+       {fromUserId, toUserId},
+       {fromUserId : toUserId, toUserId : fromUserId},
+    ],
+   })
+   if(existingconnectionRequest)
+   {
+    return res.status(400).send("connection request already exists");
+   }
     const newconnectionRequest =  new connectionRequest({
       fromUserId, toUserId, status
     });
