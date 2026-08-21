d2924697264fd9046e623a0c59074d6a6ae48af5
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-08-21 11:10:10 +0530

    adding a check to only  send a connection request to users who exist in database

--- src/routes/request.js
+++ src/routes/request.js
@@ -4,6 +4,7 @@ const {userauth} = require("../middleware/auth");
 const user = require("../models/user");
 const connectionRequest = require("../models/connectionrequest");
 const { isAbaRouting } = require("validator");
+const User = require("../models/user");
 
 
 connectionrequestRouter.post("/sendconnectionrequest", userauth, async (req, res)=>{
@@ -23,6 +24,12 @@ connectionrequestRouter.post("/request/send/:status/:toUserId", userauth, async(
    if(!allowedstatus.includes(status)){
     return res.status(400).json({message: "invalid status type " + status});
    }
+ // ek toUser naam ka instance banao, agar user databse me exist krta hai toh findbyId se mil jaega, and tum usko requestbhej paoge
+   const toUser = await User.findById(toUserId);
+   if(!toUser){// aur agar user doesnot exist in the database then badhiya tum usko request hi nhi bhej paoge
+    return res.status(404).send("user not found");
+   }
+
    // if a has sent a connection request to b once, it should not be allowed to send it again, and also b can not send request to a then
    const existingconnectionRequest = await connectionRequest.findOne({
     $or:[
