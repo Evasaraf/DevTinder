84754228d167486e48bff6b16af8ae43f3b62b71
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-08-23 12:48:19 +0530

    created an api to receive the user requests

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const userRouter = express.Router();
+const {userauth} = require("../middleware/auth");
+const connectionRequest = require("../models/connectionrequest");
+
+userRouter.get("/user/requests/received", userauth, async (req, res) => {
+    try{
+     const loggedInUser = req.user;
+     const connectionRequests= await connectionRequest.find({
+      toUserId: loggedInUser._id, 
+      status: "interested",
+     });
+     res.json({message: "data fetched successfully",
+        data : connectionRequests,
+     })
+    }
+    catch(err){
+        return res.send("error:", err.message)
+    }
+})
 
 module.exports = userRouter;
